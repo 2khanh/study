c6d9a68b1128d07e0682eb969a74920e7501ac76
khanhln-ghitz <khanhln.workspace@gmail.com>
2023-12-15 22:36:34 +0700

    [NEW] Init Project: JavaBasic

---
+++ com.w3resource.java/JavaBasic/src/main/java/com/w3resource/javabasic/JavaBasic.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.w3resource.javabasic;
+
+/**
+ *
+ * @author kailn
+ */
+public class JavaBasic {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
