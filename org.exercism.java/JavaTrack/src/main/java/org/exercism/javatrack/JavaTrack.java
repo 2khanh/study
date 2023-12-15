6d2206e5892d4d803156ec8ed87cfe94b9d6e7b1
khanhln-ghitz <khanhln.workspace@gmail.com>
2023-12-15 22:42:37 +0700

    [NEW] Init Project: JavaTrack

---
+++ org.exercism.java/JavaTrack/src/main/java/org/exercism/javatrack/JavaTrack.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package org.exercism.javatrack;
+
+/**
+ *
+ * @author kailn
+ */
+public class JavaTrack {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
